0c2577af14e9a95520272b554b528dd20865b119
Gauravsinh Pardeshi <gauravpardeshi1212@gmail.com>
2026-07-23 07:29:48 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -2,11 +2,6 @@
 ===============================================================================
 DDL Script: Create Bronze Tables
 ===============================================================================
-Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
-===============================================================================
 */
 
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
